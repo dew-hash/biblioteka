4c0acbedba331dab226d596d39cbda1c54b80e9b
dew-hash <deeeew@gmail.com>
2021-03-18 13:51:49 +0200

    Padaryta validacija slaptažodžiui.

--- app/src/main/java/com/example/coronashow/LoginActivity.java
+++ app/src/main/java/com/example/coronashow/LoginActivity.java
@@ -25,22 +25,14 @@ import android.widget.Toast;
             public void onClick(View v) {;
                 String usernameStr = username.getText().toString();
                 String passwordStr = password.getText().toString();
-                if (Validation.isUsernameValid(usernameStr) && Validation.isUsernameValid(passwordStr)) {
+                if (Validation.isUsernameValid(usernameStr) && Validation.isPasswordValid(passwordStr)) {
                     //Toast.makeText(LoginActivity.this, "Prisijungimo vardas: " + usernameStr + "\nSlaptažodis: " + passwordStr, Toast.LENGTH_LONG).show();
                     Intent goToSearchActivity = new Intent(LoginActivity.this, SearchActivity.class); //parametrai: iš kur (visad su this, nes šita klasė), į kur (visad su class)
                     startActivity(goToSearchActivity);
                 }
-                else {
-                    if (!Validation.isUsernameValid(usernameStr)) {
-                        username.setError(getResources().getString(R.string.login_invalid_username));
-                        username.requestFocus();
-                        Toast.makeText(LoginActivity.this, "Vartotojo vardas gali būti sudarytas iš 3-15 raidžių ir skaičių", Toast.LENGTH_LONG).show(); //"Vartotojo vardas gali būti sudarytas iš 3-15 raidžių ir skaičių"
-                    }
-                    if (!Validation.isPasswordValid(passwordStr)) {
-                        username.setError(getResources().getString(R.string.login_invalid_password));
-                        username.requestFocus();
-                        Toast.makeText(LoginActivity.this, "Slaptažodis gali būti sudarytas iš 5-20 raidžių, skaičių ir spec simbolių", Toast.LENGTH_LONG).show();
-                    }
+                else {    //nereikia skliaustelių salygos nurodymui, nes bus vykdoma visais kitais atvejais
+                    username.setError(getResources().getString(R.string.login_invalid_credentials));
+                    username.requestFocus();
                 }
             }
         });
--- app/src/main/java/com/example/coronashow/Validation.java
+++ app/src/main/java/com/example/coronashow/Validation.java
@@ -5,8 +5,8 @@ import java.util.regex.Pattern;
 
 public class Validation {
     public static final String USERNAME_REGEX_PATTERN = "^[a-zA-Z0-9]{3,20}$";
-    public static final String PASSWORD_REGEX_PATTERN = "^[a-zA-Z0-9.!@_]{5,20}$";
-    public static final String EMAIL_REGEX_PATTERN = "^[a-zA-Z0-9@._-]{10,50}$";
+    public static final String PASSWORD_REGEX_PATTERN = "^[a-zA-Z0-9!@_\\.]{5,20}$";
+    public static final String EMAIL_REGEX_PATTERN = "^[a-zA-Z0-9@\\._-]{10,50}$";
 
     public static boolean isUsernameValid(String username) {
         return isCredentialsValid(username, USERNAME_REGEX_PATTERN);
