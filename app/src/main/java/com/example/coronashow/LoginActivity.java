8bf3957779241a421fcb19cdd87aa64d4f094fda
dew-hash <deeeew@gmail.com>
2021-03-23 15:36:40 +0200

    Padaryta registracija. Pakeistas paveikslėlis. Paryškintos raidės. Atskirti mygtukai.

--- app/src/main/java/com/example/coronashow/LoginActivity.java
+++ app/src/main/java/com/example/coronashow/LoginActivity.java
@@ -7,24 +7,25 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
  public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
 
         EditText username = findViewById(R.id.username);
         EditText password = findViewById(R.id.password);
         Button login = findViewById(R.id.login);
+        Button register = findViewById(R.id.register);
 
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {;
                 String usernameStr = username.getText().toString();
                 String passwordStr = password.getText().toString();
+                username.setError(null);
                 if (Validation.isUsernameValid(usernameStr) && Validation.isPasswordValid(passwordStr)) {
                     //Toast.makeText(LoginActivity.this, "Prisijungimo vardas: " + usernameStr + "\nSlaptažodis: " + passwordStr, Toast.LENGTH_LONG).show();
                     Intent goToSearchActivity = new Intent(LoginActivity.this, SearchActivity.class); //parametrai: iš kur (visad su this, nes šita klasė), į kur (visad su class)
@@ -34,6 +35,14 @@ import android.widget.Toast;
                     username.setError(getResources().getString(R.string.login_invalid_credentials));
                     username.requestFocus();
                 }
+            }
+        });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goToRegisterActivity = new Intent(LoginActivity.this, RegisterActivity.class); //parametrai: iš kur (visad su this, nes šita klasė), į kur (visad su class)
+                startActivity(goToRegisterActivity);
             }
         });
 
